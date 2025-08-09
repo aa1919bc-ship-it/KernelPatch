15de0ec9e475e71407cf53a4db8086e22c6aa108
aa1919bc-ship-it <aa1919bc@gmail.com>
2025-08-09 13:52:34 +0000

    update .gitignore to include generated kernel images and optimize build.sh for parallel execution

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
           export TARGET_COMPILE=`pwd`/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
           cd kernel
-
+asdf
           export ANDROID=1
-          make
+          make -j 8
           mv kpimg kpimg-android
           mv kpimg.elf kpimg.elf-android
           make clean
